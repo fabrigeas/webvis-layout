3ca19f38ae7ebe66153baa0a38b5d18654082f1b
fabrigeas <fabrigeas@gmail.com>
2023-03-17 10:38:46 +0100

    feat: create model from input URN

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
 var context;
 
 /**
+ * add the given model to the view
+ *
+ * @param {string} dataURI - the URN | URL
+ */
+const addModel = async dataURI => {
+  try {
+    const nodeId = context.add({
+      dataURI,
+      initialProperties: {
+        enabled: true,
+      },
+    });
+
+    setModalHiddem(true);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+/**
+ *
+ * @param {SubmitEvent} event
+ */
+const submitModel = event => {
+  event.preventDefault();
+
+  const urn = event.target.querySelector('input').value;
+
+  if (urn) {
+    addModel(urn);
+    setModalHiddem(true);
+  } else {
+    alert('urn is empty');
+  }
+};
+
+/**
  * add Event handlers for given domNodes
  */
 const initEventListeners = () => {
   document.querySelector('form').addEventListener('submit', submitModel);
-  document
-    .querySelector('webvis-viewer')
-    .addEventListener('click', removeSelectedNodes);
 };
 
 /**
@@ -29,5 +63,5 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   initEventListeners();
 
-  addModel('urn:x-i3d:examples:x3d:v8');
+  // addModel('urn:x-i3d:examples:x3d:v8');
 });
