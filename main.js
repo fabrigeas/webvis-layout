ccb59b2080ab9bac87fac24725b9fc24c664e5b4
fabrigeas <fabrigeas@gmail.com>
2023-03-17 10:30:57 +0100

    feat: create add model button and dlalog

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 var context;
 
 /**
+ * add Event handlers for given domNodes
+ */
+const initEventListeners = () => {
+  document.querySelector('form').addEventListener('submit', submitModel);
+  document
+    .querySelector('webvis-viewer')
+    .addEventListener('click', removeSelectedNodes);
+};
+
+/**
+ * sets the 'hidden' attribute of the addModelModal
+ *
+ * @param {boolean} hidden
+ *
+ */
+const setModalHiddem = hidden => {
+  document.getElementById('add-model-dialog').hidden = hidden;
+};
+
+/**
  * the main of this application
  * but runs after the document is ready.
  */
 window.addEventListener('DOMContentLoaded', async () => {
   context = webvis.getContext();
+
+  initEventListeners();
+
+  addModel('urn:x-i3d:examples:x3d:v8');
 });
